7e24a0aa6547a2d48d352c71d0bef708e352ec56
Andrew Allaire <andrew.allaire@gmail.com>
2019-09-01 16:57:10 -0500

    updated version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="forex-types",
-    version="0.0.2",
+    version="0.0.3",
     author="Andrew Allaire",
     author_email="andrew.allaire@gmail.com",
     description="Basic Forex classes",
